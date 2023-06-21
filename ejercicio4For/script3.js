ef050f1d760bbd09ad99011914f2d8ecf8b79cdf
OneFatte <camiloandres798mail.com>
2023-06-20 19:23:40 -0500

    Se agrega ejercicio 5

--- ejercicio4For/script3.js
+++ ejercicio4For/script3.js
@@ -4,4 +4,7 @@ for (let i = 0; i < arreglo.length; i++) {
     if (!isNaN(arreglo[i])) {
         console.log(arreglo[i]);
     }
+    if (typeof arreglo[i] === 'number') {
+        console.log(arreglo[i]);
+    }
 }
